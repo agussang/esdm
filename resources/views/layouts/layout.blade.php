1845e11b1e8449b533604b2592037b7e7c12eda8
rofikainur336 <rofikainur336@gmail.com>
2023-03-15 09:08:38 +0700

    mesti kelalen

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="csrf_token" content="{{ csrf_token() }}" />
-        {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>{{Session::get('nama_aplikasi')}}</title>
         @include('layouts.css')
         @stack('css')
